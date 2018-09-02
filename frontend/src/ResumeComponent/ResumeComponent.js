f458fc4039e9f5fbcdf0ddb060cf4bbc5386d8ce
Tyler Whitehurst <twwhitehurst@gmail.com>
2018-09-02 12:09:59 -0400

    resume sizing

--- frontend/src/ResumeComponent/ResumeComponent.js
+++ frontend/src/ResumeComponent/ResumeComponent.js
@@ -4,7 +4,7 @@ const resumeComponent = (props) => {
   return (
     <div className='Component Light' id='Resume'>
       <h1>Resume</h1>
-      <embed src="/resources/updated_resume.pdf" type='application/pdf' />
+      <embed src="/resources/updated_resume.pdf#view=FitH" type='application/pdf' width="95%"/>
     </div>
   );
 };
