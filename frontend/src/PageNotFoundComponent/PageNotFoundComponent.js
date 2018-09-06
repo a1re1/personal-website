97ee55df7d1384ba5f424186e08832968537a32e
Tyler Whitehurst <twwhitehurst@gmail.com>
2018-09-05 22:41:08 -0400

    made 404 say that blog is wip

--- frontend/src/PageNotFoundComponent/PageNotFoundComponent.js
+++ frontend/src/PageNotFoundComponent/PageNotFoundComponent.js
@@ -4,7 +4,10 @@ const pageNotFoundComponent = () => {
   return (
     <div className='TextBox'>
       <div className="Info-card">
-        <div className='Title'>404 Page Not Found</div>
+        <div className='Title'><br />
+          404 This is not the page you were looking for. <br /><br />
+          Unless of course you are searching for my blog. That is still a WIP.
+        </div>
       </div>
     </div>
   );
