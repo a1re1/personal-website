3f2ceafae3aedb9a5d40a7e6b10c2cf82080890c
Tyler Whitehurst <twwhitehurst@gmail.com>
2019-01-30 23:19:38 -0500

    updated dependencies and fixed mountain clipping and click through

--- frontend/src/MountainComponent/MountainComponent.js
+++ frontend/src/MountainComponent/MountainComponent.js
@@ -4,38 +4,42 @@ import './mountains.css'
 const mountainComponent = (props) => {
   return (
     <div className='Mountains'>
-        <body>
-            <div className="wrap">
-                <div className="cloud-bg"></div>
-                <div className="mountain-small">
-                    <svg className="peak" width="178" height="112">
-                        <polyline points="89,0 0,112 
-                                        40,95 56,112 
-                                        89,70 110,70
-                                        125,90 145,70" fill="#81b1e4" stroke-width="1" stroke="#81b1e4"/>
-                    </svg>
-                </div> 
-                <ul className="cloud-list">
-                    <li className="cloud cloud-small cloud-light"></li> 
-                    <li className="cloud cloud-small cloud-light"></li> 
-                    <li className="cloud cloud-small cloud-light"></li> 
-                    <li className="cloud cloud-small cloud-light"></li> 
-                    <li className="cloud cloud-small cloud-light"></li> 
-                    <li className="cloud cloud-small cloud-light"></li> 
-                </ul>  
-                <div className="hill hill-right"></div>
-                <div className="hill hill-left"></div>
-                <ul className="tree-list">
-                    <li className="tree tree-dark"></li>
-                    <li className="tree tree-dark"></li>
-                    <li className="tree tree-dark"></li>
-                    <li className="tree tree-dark"></li>
-                    <li className="tree tree-light"></li>
-                    <li className="tree tree-light"></li>
-                    <li className="tree tree-light"></li>
-                </ul>  
-            </div>   
-        </body> 
+        <div className="wrap">
+            <div className="cloud-bg"></div>
+            <div className="mountain-small">
+                <svg className="peak" width="178" height="112">
+                    <polyline points="89,0 0,112 
+                                    40,95 56,112 
+                                    89,70 110,70
+                                    125,90 145,70" fill="#81b1e4" strokeWidth="1" stroke="#81b1e4"/>
+                </svg>
+            </div> 
+            <ul className="cloud-list">
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+                <li className="cloud cloud-small cloud-light"></li> 
+            </ul>  
+            <div className="hill hill-right"></div>
+            <div className="hill hill-left"></div>
+            <ul className="tree-list">
+                <li className="tree tree-dark"></li>
+                <li className="tree tree-dark"></li>
+                <li className="tree tree-dark"></li>
+                <li className="tree tree-dark"></li>
+                <li className="tree tree-light"></li>
+                <li className="tree tree-light"></li>
+                <li className="tree tree-light"></li>
+            </ul>  
+        </div>   
     </div>
   );
 };
