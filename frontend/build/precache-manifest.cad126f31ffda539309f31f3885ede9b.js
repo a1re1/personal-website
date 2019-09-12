a64cc9d64d78611bac9dc841b54596974cf4d5d4
Tyler Whitehurst <twwhitehurst@gmail.com>
2019-09-12 10:45:28 -0400

    pushing build

---
+++ frontend/build/precache-manifest.cad126f31ffda539309f31f3885ede9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542731ac4cbc3b8bc8aa32789593e2f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a525d7ec1bd7236622",
+    "url": "/static/css/main.df8f5ddc.chunk.css"
+  },
+  {
+    "revision": "53180c63463f1522d410",
+    "url": "/static/js/2.10cb24b1.chunk.js"
+  },
+  {
+    "revision": "29a525d7ec1bd7236622",
+    "url": "/static/js/main.dc0a4cc9.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
